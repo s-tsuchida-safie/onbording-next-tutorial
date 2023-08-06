a52645f5517f7dae908f525e74c0ff0e60e82d7d
s-tsuchida-safie <s-tsuchida@safie.jp>
2023-08-07 02:54:50 +0900

    fix: デフォルトレイアウトの修正

--- src/components/app/AppInitializer.tsx
+++ src/components/app/AppInitializer.tsx
@@ -1,6 +1,6 @@
-import { countUp } from '@/redux-slice/appState/slice'
 import { ReactNode, useEffect } from 'react'
 import { useDispatch } from 'react-redux'
+import { countUp } from '@/redux-slice/appState/counter'
 
 type AppInitializerProps = { children: ReactNode }
 
@@ -8,11 +8,11 @@ const AppInitializer = ({ children }: AppInitializerProps) => {
   const dispatch = useDispatch()
   useEffect(() => {
     ;(async () => {
-      const initalState = 3;
-      dispatch(countUp(initalState));
+      const initalState = 0
+      dispatch(countUp(initalState))
     })()
   }, [dispatch])
   return <>{children}</>
 }
 
-export default AppInitializer;
+export default AppInitializer
--- src/components/layout/DefaultLayout.tsx
+++ src/components/layout/DefaultLayout.tsx
@@ -1,11 +1,11 @@
 //React
 import { ReactNode } from 'react'
-
-import Header from '../app/Header'
-
-import styles from './DefaultLayout.module.scss'
 import { Provider } from 'react-redux'
 import { store } from '@/redux-slice/store'
+
+import Header from '../app/Header/Header'
+
+import styles from './DefaultLayout.module.scss'
 
 type DefaultLayoutProps = { children: ReactNode }
 
@@ -13,15 +13,15 @@ const DefaultLayout = ({ children }: DefaultLayoutProps) => {
   return (
     <>
       <Provider store={store}>
-      <div className={styles['app-default-layout']}>
-        <Header />
-        <div className={styles['app-main']}>
-          <div className={styles['app-content']}>{children}</div>
+        <div className={styles['app-default-layout']}>
+          <Header />
+          <div className={styles['app-main']}>
+            <div className={styles['app-content']}>{children}</div>
+          </div>
         </div>
-      </div>
       </Provider>
     </>
   )
 }
 
-export default DefaultLayout;
+export default DefaultLayout
